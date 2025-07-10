79f8120b1e40cbec319af0d7367841145e507776
Gabriel Smereka <102737099+gsmereka@users.noreply.github.com>
2025-07-10 17:34:13 -0300

    change script

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 // Predefined message and phone number
 const phoneNumber = "5573988511248";
-const message = "Olá, tudo bem? Queria saber mais sobre o método Nutrielev 😊";
+const message = "Olá, tudo bem? Queria saber mais sobre o método Nutrielev ‍🔥";
 
 const whatsappLink = `https://api.whatsapp.com/send/?phone=${phoneNumber}&text=${encodeURIComponent(message)}`;
 
